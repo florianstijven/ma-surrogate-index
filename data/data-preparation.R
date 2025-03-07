60663894696a1ac9283ee2de4a2e2837fcbca3a5
Florian Stijven <florian.stijven@gmail.com>
2025-03-07 13:43:06 -0800

    Add .gitkeep files to preserve directory structure

---
+++ data/data-preparation.R
@@ -0,0 +1,126 @@
+# Load required packages
+library(tidyr)
+library(dplyr)
+
+# Load the data set. Wstratum refers to the strata that determine the
+# probability of being sampled for measuring S. 
+df = read.csv("data/CrossProtocolData_moderna_tgt_FINAL.csv") %>% 
+  mutate(Wstratum = as.factor(Wstratum)) %>%
+  droplevels() 
+
+# Add variable indicating whether the patient was vaccinated with any vaccine or
+# received placebo.
+df = df %>%
+  mutate(vax = ifelse(df$A == 0, 0, 1))
+
+# Compute LODs for each trial and type of surrogate.
+LOD <- df %>% group_by(trial) %>% dplyr::summarise(
+  LLOD.Spike = min(bindSpike, na.rm = T),
+  LLOD.Neut = min(pseudoneutid50, na.rm = T)
+)
+
+# Maximum LOQs across all trials. 
+maxLLODSpike <- max(LOD$LLOD.Spike)
+maxLLODNeut <- max(LOD$LLOD.Neut)
+
+# Set all values of placebo to the max of the LLODs
+df <- df %>% mutate(bindSpike = ifelse(A == 0, maxLLODSpike, bindSpike))
+df <- df %>% mutate(pseudoneutid50 = ifelse(A == 0, maxLLODNeut, pseudoneutid50))
+
+# Truncate surrogate measurements to the LOD across trials. 
+df <- df %>% mutate(bindSpike = ifelse(Delta == T &
+                                         bindSpike < maxLLODSpike, maxLLODSpike, bindSpike))
+df <- df %>% mutate(
+  pseudoneutid50 = ifelse(
+    Delta == T & pseudoneutid50 < maxLLODNeut,
+    maxLLODNeut,
+    pseudoneutid50
+  )
+)
+
+# Rowwise deletetion for missing BMI values. 
+df <- df %>% filter(!is.na(BMI))
+
+# Compute strata for the case-cohort sampling. 
+df = df %>%
+  mutate(
+    CC_stratum = paste(trial, Y, A, Wstratum, sep = "_"),
+    CC_stratum = ifelse(A == 0, "Placebo", CC_stratum),
+    CC_stratum = as.factor(CC_stratum)
+  )
+
+# Drop variables which are currently not used in any analyses.
+df = df %>%
+  select(
+    -X,
+    -Ptid,
+    -HighRiskInd,
+    -CalendarDateEnrollment,
+    -ph1,
+    -WhiteNonHispanic,
+    -STUDYID,
+    -USUBJID, 
+    -protocol, 
+    -hd_mean,
+    -Country, 
+    -SUBJID, 
+    -EarlyInd,
+    -TTY,
+    -Sex,
+    -Wstratum
+  )
+
+# Add label for the trials.
+df = df %>%
+  mutate(trial.lbl = sapply(
+    X = trial,
+    FUN = function(x) {
+      switch (
+        as.character(x),
+        "1" = "Moderna",
+        "2" = "AstraZeneca",
+        "3" = "Novavax",
+        "4" = "J&J (Brazil)",
+        "5" = "J&J (Colombia)",
+        "6" = "J&J (S. America)",
+        "7" = "J&J (S. Africa)",
+        "8" = "J&J (USA)"
+      )
+    }
+  ))
+
+# For the non-JnJ trials, BMI should be categorized into <18.5, [18.5, 25), [25,
+# 30), >= 30.
+df = df %>%
+  mutate(
+    BMI_underweight = as.integer(ifelse(
+      stringr::str_detect(trial.lbl, "J&J"), BMI == 1, BMI < 18.5
+    )),
+    BMI_normal = as.integer(ifelse(
+      stringr::str_detect(trial.lbl, "J&J"),
+      BMI == 2,
+      BMI >=  18.5 &
+        BMI < 25
+    )),
+    BMI_overweight = as.integer(ifelse(
+      stringr::str_detect(trial.lbl, "J&J"), BMI == 3, BMI >= 25 &
+        BMI < 30
+    )),
+    BMI_obese = as.integer(ifelse(
+      stringr::str_detect(trial.lbl, "J&J"), BMI == 4, BMI > 30
+    )),
+    BMI_underweight_normal = as.integer(ifelse(
+      stringr::str_detect(trial.lbl, "J&J"), BMI <= 2, BMI < 25
+    ))
+  )
+
+df = df %>%
+  filter(!is.na(risk_score)) 
+
+# Remove variables that are used in any of the current analyses.
+df = df %>%
+  select(-wt.est, -BMI)
+
+
+# Save processed data set.
+write.csv(df, "data/processed_data.csv")
---
+++ data/generate-synthetic-data.R
@@ -0,0 +1,119 @@
+library(synthpop)
+library(dplyr)
+
+set.seed(1)
+
+df = read.csv("data/processed_data.csv") %>%
+  select(-X)
+
+# Recode variables into the correct format for the the synthpop package. Factors
+# should be factors, not 0/1 dummy variables. 
+df = df %>%
+  mutate(CC_stratum = as.factor(CC_stratum),
+         BMI_stratum = case_when(
+           BMI_underweight == 1 ~ "Underweight",
+           BMI_normal == 1 ~ "Normal",
+           BMI_overweight == 1 ~ "Overweight",
+           BMI_obese == 1 ~ "Obese",
+           .default = NA
+         ),
+         BMI_stratum = as.factor(BMI_stratum),
+         trial.lbl = as.factor(trial.lbl))
+
+# Some patients have titer values event though they were not sampled in the
+# case-cohort sampling. We set the titer values for these patients to NA.
+df = df %>%
+  mutate(
+    bindSpike = ifelse((Delta == 0) & (vax == 1), NA, bindSpike),
+    pseudoneutid50 = ifelse((Delta == 0) & (vax == 1), NA, pseudoneutid50)
+  )
+
+
+# Check whether the synthpop recognizes the variables correctly. 
+codebook.syn(df)
+
+# Order in which variables are predicted. Variables which are not used for
+# predictions (i.e., as predictors) and which are not predicted themselves,
+# should not be in this character vector. The value of such variables is just
+# kept unchanged.
+visit.sequence.ini <- c(
+  "Delta",
+  "vax",
+  "age.geq.65",
+  "BMI_stratum",
+  "CC_stratum",
+  "risk_score",
+  "Age",
+  "bindSpike",
+  "pseudoneutid50",
+  "Y"
+)
+
+# Method used to predict each variable in the data set. Note that we have on
+# element for each variable in the data set and the order of the character
+# vector below corresponds to the order of the columns in the data set. An empty
+# string means that that variable is not predicted (i.e., the original values
+# are kept in the  synthetic data set).
+method.ini <- c("",
+                "ctree",
+                "ctree",
+                "",
+                "ctree",
+                "ctree",
+                "",
+                "ctree",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "")
+
+# Generate synthetic data set. The variables are generated in each trial
+# separately. We don't use the syn.strata() function because that function
+# samples from the strata; consequently, the trial-specific sample sizes are not
+# the same in the synthetic data as in the original data. 
+sds_list = lapply(
+  X = levels(df$trial.lbl),
+  FUN = function(trial) {
+    sds <- syn(
+      data = df,
+      visit.sequence = visit.sequence.ini,
+      method = method.ini,
+      m = 1,
+      # Variables that are not predicted are kept in the synthetic data set.
+      drop.not.used = FALSE,
+      drop.pred.only = FALSE,
+      # The CC_stratum variable has 58 categories, so we have to ensure that that
+      # variable is kept as a categorical variable.
+      maxfaclevels = 60,
+      minnumlevels = 20,
+      # The titers should be missing for patients with Delta == 0 in the vaccine
+      # group.
+      rules = list(bindSpike = "(Delta == 0) & vax == 1", 
+                   pseudoneutid50 = "(Delta == 0) & vax == 1",
+                   A = "vax == 0"),
+      rvalues = list(bindSpike = NA, pseudoneutid50 = NA, A = 0)
+    )
+  }
+)
+
+# Combine the synthetic data sets contains in the sds objects in the list
+# generated above into a single data set.
+synthetic_df = lapply(
+  X = sds_list,
+  FUN = function(x) x$syn
+) %>%
+  bind_rows() %>%
+  # Remove helper variables that were not present in the original data from
+  # which we started in this script.
+  select(-BMI_stratum)
+
+# Save synthetic data set.
+write.csv(synthetic_df, file = "data/processed_data_synthetic.csv")
+
+
