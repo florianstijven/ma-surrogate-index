d1e98b4122bd15dc18888e3144c7c75d8aef6908
florianstijven <florian.stijven@gmail.com>
2025-07-01 11:31:10 +0200

    Run simulations on VSC

--- run-application-synthetic.sh
+++ run-application-synthetic.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes=1
 #SBATCH --output=par-%J.out
-#SBATCH --cpus-per-task=36
+#SBATCH --ntasks=1 --cpus-per-task=36 --nodes=1
+#SBATCH --time=3:00:00
+#SBATCH --cluster=wice
 #SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
+#SBATCH -A lp_doctoralresearch
 
 ml fhR/4.4.0-foss-2023b
 export OMP_NUM_THREADS=1
