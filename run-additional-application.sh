3f91c70636244e2e1c5597640cc6fe39be9597ba
florianstijven <florian.stijven@gmail.com>
2026-05-20 10:12:44 +0200

    update

--- run-additional-application.sh
+++ run-additional-application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output=par-%J.out
 #SBATCH --ntasks=1 --cpus-per-task=72 --nodes=1
-#SBATCH --time=02:00:00
+#SBATCH --time=00:45:00
 #SBATCH --cluster=wice
 #SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
 #SBATCH -A lp_doctoralresearch
