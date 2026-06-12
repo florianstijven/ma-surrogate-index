fb687dd9af9e9404f509aeabe20037ecf1f08364
florianstijven <florian.stijven@gmail.com>
2026-06-12 22:34:16 +0200

    update script for HPC

--- run-additional-application-2.sh
+++ run-additional-application-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output=par-%J.out
-#SBATCH --ntasks=1 --cpus-per-task=72 --nodes=1
-#SBATCH --time=00:45:00
+#SBATCH --ntasks=1 --cpus-per-task=36 --nodes=1
+#SBATCH --time=03:45:00
 #SBATCH --cluster=wice
 #SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
 #SBATCH -A lp_doctoralresearch
@@ -10,7 +10,7 @@ module load cluster/wice/batch
 module load R/4.4.2-gfbf-2024a
 module load R-bundle-CRAN/2024.11-foss-2024a
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=3
 
 # Rscript -e "install.packages(c('BBmisc', 'delayed', 'caret')"
 # Rscript -e "install.packages('tlverse-sl3-v1.4.4-243-g0e8f236.tar.gz', type = 'source', repos = NULL)"
